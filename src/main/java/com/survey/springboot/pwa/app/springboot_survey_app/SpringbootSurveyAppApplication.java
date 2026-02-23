5618179b839ce6a791df9cf4f7418f487c20c328
Gabriel Infante <gabriel.infante@uptc.edu.co>
2026-02-23 15:13:28 -0500

    Configuracion inicial

---
+++ src/main/java/com/survey/springboot/pwa/app/springboot_survey_app/SpringbootSurveyAppApplication.java
@@ -0,0 +1,13 @@
+package com.survey.springboot.pwa.app.springboot_survey_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootSurveyAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootSurveyAppApplication.class, args);
+	}
+
+}
